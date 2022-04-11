eb4342bb97607d96b0a2ecd9bc529a843f747896
DomenicoAgostino <agostino.domenico96@gmail.com>
2022-04-11 16:59:11 +0200

    Inizio esercizio su js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere (creare quindi una lista di utenti autorizzati),
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+const email = ["boolean@gmail.com", "dome@gmail.com", "superman@gmail.com"]
+console.log("email");
+
+const username = prompt("Inserisci la tua mail");
+console.log(username);
+
