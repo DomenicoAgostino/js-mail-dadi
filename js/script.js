6883ee7d1df698a613d4f1e1d414104b5994a85a
DomenicoAgostino <agostino.domenico96@gmail.com>
2022-04-12 16:23:21 +0200

    Aggiunta js per la prima parte mail

--- js/script.js
+++ js/script.js
@@ -13,3 +13,27 @@ console.log("email");
 const username = prompt("Inserisci la tua mail");
 console.log(username);
 
+let mailRegistrata = false;
+
+
+for (let i = 0; i < email.length; i++) {
+  console.log("email");
+}
+
+for ( let i = 0; i < email.length; i++) {
+  if(email[i] === username){
+    mailRegistrata = true;
+  } 
+}
+
+
+if(mailRegistrata){
+  loginMail = "Utente Loggato";
+} else {
+  loginMail = "Utente Non Registrato"
+}
+
+document.getElementById("login").innerHTML = loginMail;
+
+
+
